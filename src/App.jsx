8b9ffffecf2c7174bc21aa17cff9d4d64731bb61
Nithika Mendon <nithikamendon2@gamil.com>
2026-09-06 11:15:06 +0530

    Inital Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+export default function App(){
+  return <form className="new-item-Form">
+    <div className="form-item">
+      <label htmlFor="item">New Item</label>
+
+    </div>
+  </form>
+}
