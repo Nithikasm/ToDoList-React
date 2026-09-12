aeae0b62c08961b0b5ac342d3d67e4e3c83ad49c
Nithika Mendon <nithikamendon2@gamil.com>
2026-09-12 13:43:46 +0530

    Page Format Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react"
 import "./style.css"
 export default function App(){
-  return <form className="new-item-Form">
-    <div className="form-item">
+  const [newItem,setNewItem] = useState("")
+   return (
+   <>
+   <form className="new-item-form">
+    <div className="form-row">
       <label htmlFor="item">New Item</label>
-
+      <input 
+        value={newItem}
+        onChange={e=>setNewItem(e.target.value)}
+        type="text" id="item" />
     </div>
+    <button className="btn">ADD</button>
   </form>
+
+    <h1 className="Header">To Do List</h1>
+    <ul className="list">
+      <li>
+        <label> 
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label> 
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </>
+   )
 }
